4ba37c27be2173afcd88e8602fad240550f66851
davidMiskic <david.miskic@gmail.com>
2021-12-18 16:56:20 +0100

    openapi

--- api/src/main/java/si/fri/rso2021/workers/api/WorkerAPI.java
+++ api/src/main/java/si/fri/rso2021/workers/api/WorkerAPI.java
@@ -1,8 +1,17 @@
 package si.fri.rso2021.workers.api;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@OpenAPIDefinition(info = @Info(title = "Worker API", version = "v1",
+        license = @License(name = "David"), description = "API for managing worker data."),
+        servers = @Server(url = "http://localhost:8080/"))
 @ApplicationPath("/v1")
 public class WorkerAPI extends Application {
 
--- api/src/main/java/si/fri/rso2021/workers/api/resources/WorkerResource.java
+++ api/src/main/java/si/fri/rso2021/workers/api/resources/WorkerResource.java
@@ -18,6 +18,14 @@ import javax.ws.rs.core.UriInfo;
 import java.util.List;
 import java.util.logging.Logger;
 
+import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import si.fri.rso2021.workers.models.objects.Worker;
 import si.fri.rso2021.workers.services.beans.workerBean;
 
@@ -26,6 +34,7 @@ import si.fri.rso2021.workers.services.beans.workerBean;
 @Path("/workers")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@CrossOrigin(supportedMethods = "GET, POST, PUT, DELETE")
 public class WorkerResource {
     private Logger log = Logger.getLogger(WorkerResource.class.getName());
 
@@ -35,6 +44,12 @@ public class WorkerResource {
     @Context
     protected UriInfo uriInfo;
 
+    @Operation(description = "Get all worker data.", summary = "Get all workers")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of worker data",
+                    content = @Content(schema = @Schema(implementation = Worker.class, type = SchemaType.ARRAY))
+            )})
     @GET
     public Response getWorkerData() {
 
@@ -43,6 +58,16 @@ public class WorkerResource {
         return Response.status(Response.Status.OK).entity(workers).build();
     }
 
+
+    @Operation(description = "Get one worker data.", summary = "Get one worker")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Worker data",
+                    content = @Content(schema = @Schema(implementation = Worker.class, type = SchemaType.OBJECT))
+            ),
+            @APIResponse( responseCode = "404",
+                    description = "Worker with id does not exist"
+            )})
     @GET
     @Path("/{id}")
     public Response getWorkerData_byId(@PathParam("id") Integer id) {
@@ -53,6 +78,14 @@ public class WorkerResource {
         return Response.status(Response.Status.OK).entity(w).build();
     }
 
+    @Operation(description = "Create one worker.", summary = "Create one worker")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Worker created"
+            ),
+            @APIResponse( responseCode = "400",
+                    description = "Creation not successful"
+            )})
     @POST
     public Response createWorker(Worker w) {
         if (w.getFirstName() == null || w.getAddress() == null) {
@@ -61,9 +94,17 @@ public class WorkerResource {
         else {
             w = workerbean.createWorker(w);
         }
-        return Response.status(Response.Status.CONFLICT).entity(w).build();
+        return Response.status(Response.Status.CREATED).entity(w).build();
     }
 
+    @Operation(description = "Update one worker data.", summary = "Update one worker")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Worker data updated"
+            ),
+            @APIResponse( responseCode = "304",
+                    description = "Worker with id does not exist"
+            )})
     @PUT
     @Path("/{id}")
     public Response putWorker(@PathParam("id") Integer id, Worker w){
@@ -74,6 +115,14 @@ public class WorkerResource {
         return Response.status(Response.Status.NOT_MODIFIED).build();
     }
 
+    @Operation(description = "Delete one worker.", summary = "Delete worker")
+    @APIResponses({
+            @APIResponse(responseCode = "204",
+                    description = "Worker deleted"
+            ),
+            @APIResponse( responseCode = "404",
+                    description = "Worker not found"
+            )})
     @DELETE
     @Path("{id}")
     public Response deleteWorker(@PathParam("id") Integer id){
